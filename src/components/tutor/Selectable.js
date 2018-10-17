22f8091a17947cf5ca23e759d43a8af5b66d24f2
lcpunch <lcpunchirolli@gmail.com>
2018-10-17 11:39:40 -0400

    heroku

--- src/components/tutor/Selectable.js
+++ src/components/tutor/Selectable.js
@@ -1,9 +1,43 @@
 import React from 'react'
 import BigCalendar from 'react-big-calendar'
+import TextField from "@material-ui/core/TextField";
+import { reduxForm, Field } from 'redux-form';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+import TimePicker from 'rc-time-picker';
+import moment from 'moment';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+import 'rc-time-picker/assets/index.css';
 
 const propTypes = {}
+const now = moment().hour(12).minute(0);
 
-const events = [
+function onChange(value) {
+  value.format("HH:mm");
+}
+
+function generateOptions(length, excludedOptions) {
+  const arr = [];
+  for (let value = 0; value < length; value++) {
+    if (excludedOptions.indexOf(value) < 0) {
+      arr.push(value);
+    }
+  }
+  return arr;
+}
+
+function disabledMinutes(h) {
+  return generateOptions(60, [0]);
+}
+
+function disabledHours() {
+  return [0, 1, 2, 3, 4, 5, 6, 22, 23];
+}
+
+
+/*const events = [
     {
       id: 0,
       title: 'All Day Event very long title',
@@ -108,49 +142,118 @@ const events = [
       start: new Date(new Date().setHours(new Date().getHours() - 3)),
       end: new Date(new Date().setHours(new Date().getHours() + 3)),
     },
-  ];
+  ];*/
+
+const events=[
+  {
+    id: 10,
+    start: new Date(2018, 9, 3, 13, 0, 0),
+    end: new Date(2018, 9, 3, 13, 0, 0),
+    title: 'tutorat'
+  }
+];
 
 class Selectable extends React.Component {
   constructor(...args) {
     super(...args)
 
-    this.state = { events }
+    this.state = {
+      selectedDate: "",
+      events,
+      modal: false
+    }
+
+    this.toggle = this.toggle.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
+
   handleSelect = ({ start, end }) => {
-    const title = window.prompt('New Event name')
-    if (title)
-      this.setState({
-        events: [
-          ...this.state.events,
-          {
-            start,
-            end,
-            title,
-          },
-        ],
-      })
+    this.setState({
+      selectedDate: moment(start).format("MM/DD/YYYY"),
+      modal: true
+    });
+  }
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
+  onSubmit = (formProps) => {
+    const time = document.getElementById('time').value;
+    const event = {
+      start: new Date(moment(this.state.selectedDate).add(parseInt(time.substring(0, 2)), 'hours').format('YYYY-MM-DD HH:mm')),
+      end: new Date(moment(this.state.selectedDate).add(parseInt(time.substring(0, 2))+1, 'hours').format('YYYY-MM-DD HH:mm')),
+      title: 'tutorat'
+    }
+    console.log(event);
+    //TODO: salvar e retornar os dados. aqui nao tenho acesso ao state sei la pq
+    this.props.saveCalendar(event);
+  };
+
+  renderHour() {
+    return <TimePicker
+              showSecond={false}
+              id="time"
+              defaultValue={now}
+              className="xxx"
+              onChange={onChange}
+              disabledHours={disabledHours}
+              disabledMinutes={disabledMinutes}
+            />;
   }
 
   render() {
     const { localizer } = this.props
+    const { handleSubmit } = this.props;
+
     return (
-      <>
+      <div>
         <BigCalendar
           selectable
+          style={{height: '900px'}}
+          views={{month: true}}
           localizer={localizer}
           events={this.state.events}
-          defaultView={BigCalendar.Views.WEEK}
-          scrollToTime={new Date(1970, 1, 1, 6)}
-          defaultDate={new Date(2015, 3, 12)}
+          defaultView={BigCalendar.Views.MONTH}
+          defaultDate={new Date()}
           onSelectEvent={event => alert(event.title)}
           onSelectSlot={this.handleSelect}
         />
-      </>
+        <Modal backdrop="static" isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+          <ModalHeader toggle={this.toggle}>Choisissez une heure de début</ModalHeader>
+            <ModalBody>
+              <form onSubmit={handleSubmit(this.onSubmit)}>
+                <div className="form-group">
+                  <Field
+                    name="hrfinish"
+                    className="form-control"
+                    component={this.renderHour}
+                    autoComplete="none"
+                    />
+                </div>
+                <button className="btn btn-primary">
+                  Sauvegarder
+                </button>
+              </form>
+            </ModalBody>
+        </Modal>
+      </div>
     )
   }
 }
 
 Selectable.propTypes = propTypes
 
-export default Selectable
+
+function mapStateToProps(state) {
+  return { errorMessage: state.auth.errorMessage };
+}
+
+export default compose(
+  connect(mapStateToProps, actions),
+  reduxForm({ form:'signin' })
+)(Selectable);
+
